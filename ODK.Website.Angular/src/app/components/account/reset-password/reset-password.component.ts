5812a2dbdc1ab55fd219858b667374022ea67706
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2020-01-01 15:47:42 +0000

    (account): add password reset feedback

--- ODK.Website.Angular/src/app/components/account/reset-password/reset-password.component.ts
+++ ODK.Website.Angular/src/app/components/account/reset-password/reset-password.component.ts
@@ -4,7 +4,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subject } from 'rxjs';
 
 import { appPaths } from 'src/app/routing/app-paths';
+import { appUrls } from 'src/app/routing/app-urls';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { Chapter } from 'src/app/core/chapters/chapter';
+import { ChapterService } from 'src/app/services/chapters/chapter.service';
 import { FormViewModel } from 'src/app/modules/forms/components/form/form.view-model';
 import { ServiceResult } from 'src/app/services/service-result';
 import { TextInputFormControlViewModel } from 'src/app/modules/forms/components/inputs/text-input-form-control/text-input-form-control.view-model';
@@ -19,21 +22,30 @@ export class ResetPasswordComponent implements OnInit, OnDestroy {
   constructor(private changeDetector: ChangeDetectorRef,
     private route: ActivatedRoute,
     private router: Router,
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private chapterService: ChapterService
   ) {
   }
 
   form: FormViewModel;
-
-  private controls: {
-    password: TextInputFormControlViewModel;
+  links: {
+    login: string;
   };
+  updated: boolean;
+
+  private chapter: Chapter;
   private formCallback: Subject<string[]> = new Subject<string[]>();
+  private formControls: {
+    password: TextInputFormControlViewModel;
+  };  
   private token: string;
 
   ngOnInit(): void {
     this.token = this.route.snapshot.queryParamMap.get(appPaths.password.reset.queryParams.token);
-
+    this.chapter = this.chapterService.getActiveChapter();
+    this.links = {
+      login: appUrls.login(this.chapter)
+    };
     this.buildForm();
   }
 
@@ -42,14 +54,16 @@ export class ResetPasswordComponent implements OnInit, OnDestroy {
   }
 
   onFormSubmit(): void {
-    this.authenticationService.completePasswordReset(this.controls.password.value, this.token).subscribe((result: ServiceResult<void>) => {
+    this.authenticationService.completePasswordReset(this.formControls.password.value, this.token).subscribe((result: ServiceResult<void>) => {
       this.formCallback.next(result.messages);
+      this.updated = true;
+      this.form = null;
       this.changeDetector.detectChanges();
     });
   }
 
   private buildForm(): void {
-    this.controls = {
+    this.formControls = {
       password: new TextInputFormControlViewModel({
         id: 'password',
         inputType: 'password',
@@ -61,13 +75,14 @@ export class ResetPasswordComponent implements OnInit, OnDestroy {
         }
       })
     };
+
     this.form = {
       buttons: [
         { text: 'Reset' }
       ],
       callback: this.formCallback,
       controls: [
-        this.controls.password
+        this.formControls.password
       ]
     };
   }
