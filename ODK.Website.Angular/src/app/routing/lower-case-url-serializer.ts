1f756cdca5261ce03719e2712bddcb34d065184c
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2020-03-14 08:30:16 +0000

    Remove console logging

--- ODK.Website.Angular/src/app/routing/lower-case-url-serializer.ts
+++ ODK.Website.Angular/src/app/routing/lower-case-url-serializer.ts
@@ -4,11 +4,8 @@ import { DefaultUrlSerializer, UrlTree } from '@angular/router';
 @Injectable()
 export class LowerCaseUrlSerializer extends DefaultUrlSerializer {
   parse(url: string): UrlTree {
-    console.log('url', url);
     const parts: string[] = url.split('?');
     const lowerCaseRoute: string = parts[0].toLocaleLowerCase() + (parts[1] ? `?${parts[1]}` : '');
-    const parsed: UrlTree = super.parse(lowerCaseRoute);
-    console.log('parsed', parsed);
-    return parsed;
+    return super.parse(lowerCaseRoute);
   }
 }
