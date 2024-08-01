c2fcaa6ad6eff869b5e2f94d25ebd0bfc448793c
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2024-08-01 12:05:32 +0100

    load theme from head

---
+++ ODK.Web.Razor/wwwroot/js/odk.themes.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    const root = document.querySelector('[data-theme-root]');
+    const buttonContainer = document.querySelector('[data-theme-selector-container]');
+
+    const buttons = buttonContainer ? {
+        light: buttonContainer.querySelector('[data-theme-selector="light"]'),
+        dark: buttonContainer.querySelector('[data-theme-selector="dark"]')
+    } : null;
+
+    const setTheme = (theme) => {
+        root.setAttribute('data-bs-theme', theme);
+        localStorage.setItem('odk.theme', theme);
+
+        if (!buttons) {
+            return;
+        }
+
+        if (theme === 'dark') {
+            buttons.dark.classList.add('d-none');
+            buttons.light.classList.remove('d-none');
+        } else {
+            buttons.dark.classList.remove('d-none');
+            buttons.light.classList.add('d-none');
+        }
+    };
+
+    const theme = localStorage.getItem('odk.theme');
+    if (theme === 'dark') {
+        setTheme('dark');
+    }
+
+    if (buttons) {
+        buttons.dark.addEventListener('click', () => {
+            setTheme('dark');
+        });
+
+        buttons.light.addEventListener('click', () => {
+            setTheme('light');
+        });
+    }
+})();
--- ODK.Web.Razor/wwwroot/js/site.js
+++ ODK.Web.Razor/wwwroot/js/site.js
@@ -1,5 +1,4 @@
 ﻿(function () {
-    bindThemes();
     bindFeaturePopovers();
     bindForms();
     bindMaps();
@@ -152,49 +151,7 @@
                 popover.show();
             }
         });        
-    }
-
-    function bindThemes() {
-        const root = document.querySelector('[data-theme-root]');
-        const buttonContainer = document.querySelector('[data-theme-selector-container]');
-
-        const buttons = buttonContainer ? {
-            light: buttonContainer.querySelector('[data-theme-selector="light"]'),
-            dark: buttonContainer.querySelector('[data-theme-selector="dark"]')
-        } : null;
-
-        const setTheme = (theme) => {
-            root.setAttribute('data-bs-theme', theme);
-            localStorage.setItem('odk.theme', theme);
-
-            if (!buttons) {
-                return;
-            }
-
-            if (theme === 'dark') {
-                buttons.dark.classList.add('d-none');
-                buttons.light.classList.remove('d-none');
-            } else {
-                buttons.dark.classList.remove('d-none');
-                buttons.light.classList.add('d-none');
-            }                                    
-        };
-
-        const theme = localStorage.getItem('odk.theme');
-        if (theme === 'dark') {            
-            setTheme('dark');
-        }
-
-        if (buttons) {
-            buttons.dark.addEventListener('click', () => {
-                setTheme('dark');
-            });
-
-            buttons.light.addEventListener('click', () => {
-                setTheme('light');
-            });
-        }        
-    }
+    }    
 
     function bindTooltips() {
         const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
