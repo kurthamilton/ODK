e0732b0537095452ec501594d6817d40989227cf
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2024-08-05 18:32:31 +0100

    Authorize members views

--- ODK.Services/Members/MemberViewModelService.cs
+++ ODK.Services/Members/MemberViewModelService.cs
@@ -18,15 +18,15 @@ public class MemberViewModelService : IMemberViewModelService
         var chapter = await _unitOfWork.ChapterRepository.GetByName(chapterName).RunAsync();
         OdkAssertions.Exists(chapter);
 
-        var (members, chapterProperties, memberProperties) = await _unitOfWork.RunAsync(
+        var (currentMember, members, chapterProperties, memberProperties) = await _unitOfWork.RunAsync(
+            x => x.MemberRepository.GetById(currentMemberId),
             x => x.MemberRepository.GetByChapterId(chapter.Id, [currentMemberId, memberId]),
             x => x.ChapterPropertyRepository.GetByChapterId(chapter.Id),
             x => x.MemberPropertyRepository.GetByMemberId(memberId, chapter.Id));
-
-        var currentMember = members.FirstOrDefault(x => x.Id == currentMemberId);
+        
         var member = members.FirstOrDefault(x => x.Id == memberId);
 
-        OdkAssertions.Exists(currentMember);
+        OdkAssertions.MemberOf(currentMember, chapter.Id);
         OdkAssertions.Exists(member);
 
         return new MemberPageViewModel
@@ -40,13 +40,16 @@ public class MemberViewModelService : IMemberViewModelService
     }
 
     public async Task<MembersPageViewModel> GetMembersPage(Guid currentMemberId, string chapterName)
-    {
+    {        
         var chapter = await _unitOfWork.ChapterRepository.GetByName(chapterName).RunAsync();
         OdkAssertions.Exists(chapter);
 
-        var members = await _unitOfWork.MemberRepository.GetByChapterId(chapter.Id).RunAsync();
-        var member = members.FirstOrDefault(x => x.Id == currentMemberId);
-        OdkAssertions.MemberOf(member, chapter.Id);
+        // get current member separately as they might be hidden from the list of members
+        var (members, currentMember) = await _unitOfWork.RunAsync(
+            x => x.MemberRepository.GetByChapterId(chapter.Id),
+            x => x.MemberRepository.GetById(currentMemberId));
+        
+        OdkAssertions.MemberOf(currentMember, chapter.Id);
 
         return new MembersPageViewModel
         {
