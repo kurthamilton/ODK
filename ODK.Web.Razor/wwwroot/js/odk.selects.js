680cc279de933ccf296ae3279881de7ef5897f22
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2024-08-11 08:17:02 +0100

    (time zone select): use searchable dropdown

--- ODK.Web.Razor/wwwroot/js/odk.selects.js
+++ ODK.Web.Razor/wwwroot/js/odk.selects.js
@@ -1,5 +1,5 @@
 ﻿(function () {
-    const $selects = document.querySelectorAll('select[multiple]');
+    const $selects = document.querySelectorAll('select[multiple],select[data-searchable]');
     $selects.forEach($select => {
         new SlimSelect({
             select: $select
