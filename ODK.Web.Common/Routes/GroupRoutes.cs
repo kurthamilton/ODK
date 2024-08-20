00417ff4f9e488cd76d4eaf8e907634951de2494
Kurt Hamilton <kurt@littlewheel.com>
2024-08-20 23:45:23 +0100

    WIP

--- ODK.Web.Common/Routes/GroupRoutes.cs
+++ ODK.Web.Common/Routes/GroupRoutes.cs
@@ -10,6 +10,7 @@ public class GroupRoutes
     public string Index() => "/groups";
     public string Join(Chapter chapter) => GroupPath(chapter, "/join");
     public string Members(Chapter chapter) => GroupPath(chapter, "/members");
+    public string Profile(Chapter chapter) => GroupPath(chapter, "/profile");
     public string Questions(Chapter chapter) => GroupPath(chapter, "/faq");
 
     private string GroupPath(Chapter chapter, string path) => $"{Group(chapter)}{path}";
