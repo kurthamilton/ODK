eb894cce4fb27ddb6d1174fe762d0284d53d937f
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2024-09-03 23:43:57 +0100

    Panels PoC

---
+++ ODK.Web.Razor/Models/Components/PanelViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace ODK.Web.Razor.Models.Components;
+
+public class PanelViewModel
+{
+    public required Func<object?, IHtmlContent> ContentFunc { get; init; }
+
+    public required Func<object?, IHtmlContent> TitleFunc { get; init; }
+}
