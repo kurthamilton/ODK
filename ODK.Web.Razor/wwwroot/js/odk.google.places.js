c7d7ff6c1a9f071b3351b533fa6f85d94d1aeb5b
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2025-11-05 22:36:46 +0000

    Set lat/long on map search text box change

--- ODK.Web.Razor/wwwroot/js/odk.google.places.js
+++ ODK.Web.Razor/wwwroot/js/odk.google.places.js
@@ -10,33 +10,25 @@
 
     function bindInputs() {
         $inputs.forEach($input => {
-            const $container = $input.closest('[data-location-container]');
-            const $lat = $container.querySelector('[data-location-lat]');
-            const $long = $container.querySelector('[data-location-long]');
-            const $latlong = $container.querySelector('[data-location-latlong]');
-            const $name = $container.querySelector('[data-location-name]');
+            const $container = $input.closest('[data-location-container]');            
 
             const options = {
                 fields: ['address_components', 'geometry'],
                 strictBounds: false,
             };
 
-            const autocomplete = new google.maps.places.Autocomplete($input, options);            
+            // whether or not a place has been selected using autocomplete
+            let placeSelected = false;
+
+            const geocoder = new google.maps.Geocoder();
+            const autocomplete = new google.maps.places.Autocomplete($input, options);
             google.maps.event.addListener(autocomplete, 'place_changed', () => {
-                const place = autocomplete.getPlace();
+                placeSelected = true;
+
+                const place = autocomplete.getPlace();                
                 const location = place.geometry.location;
-                const [lat, long] = [location.lat(), location.lng()];
-                $lat.value = lat;
-                $long.value = long;                                
-
-                if ($latlong) {
-                    $latlong.value = `${lat},${long}`;
-                    $latlong.dispatchEvent(new Event('change'));
-                }                
-
-                if ($name) {
-                    $name.value = $input.value;
-                }
+                const [lat, long] = [location.lat(), location.lng()];                
+                setLocation($container, lat, long);
             });
 
             $input.addEventListener('keydown', e => {
@@ -44,7 +36,53 @@
                 if (e.keyCode === 13) {
                     e.preventDefault();
                 }
-            }); 
+            });
+
+            // set lat long when address is entered manually and not selected from autocomplete
+            $input.addEventListener('change', () => {
+                const value = $input.value;
+
+                // Wait briefly to see if autocomplete triggers
+                setTimeout(() => {
+                    if (placeSelected) {
+                        // place was selected using autocomplete
+                        placeSelected = false;
+                        return;
+                    }
+
+                    const address = value.trim();
+                    if (!address) {
+                        return;                        
+                    }
+
+                    geocoder.geocode({ address }, (results, status) => {
+                        if (status === 'OK' && results[0]) {
+                            const lat = results[0].geometry.location.lat();
+                            const long = results[0].geometry.location.lng();
+                            setLocation($container, lat, long);
+                        }
+                    });
+                }, 100);
+            });
         });
+    }
+
+    function setLocation($container, lat, long) {
+        const $lat = $container.querySelector('[data-location-lat]');
+        const $long = $container.querySelector('[data-location-long]');
+        const $latlong = $container.querySelector('[data-location-latlong]');
+        const $name = $container.querySelector('[data-location-name]');
+
+        $lat.value = lat;
+        $long.value = long;
+
+        if ($latlong) {
+            $latlong.value = `${lat},${long}`;
+            $latlong.dispatchEvent(new Event('change'));
+        }
+
+        if ($name) {
+            $name.value = $input.value;
+        }
     }
 })();
