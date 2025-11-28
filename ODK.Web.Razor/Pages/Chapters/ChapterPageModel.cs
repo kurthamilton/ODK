2a705ef28e768e63d4ca3b529b0132f274af25fa
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2025-11-28 07:18:51 +0000

    WIP

--- ODK.Web.Razor/Pages/Chapters/ChapterPageModel.cs
+++ ODK.Web.Razor/Pages/Chapters/ChapterPageModel.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using ODK.Core;
 using ODK.Core.Chapters;
+using ODK.Services;
 
 namespace ODK.Web.Razor.Pages.Chapters;
 
 public abstract class ChapterPageModel : OdkPageModel
 {    
     public Chapter Chapter { get; private set; } = null!;
-    
+
+    public MemberChapterServiceRequest MemberChapterServiceRequest()
+        => new MemberChapterServiceRequest(Chapter.Id, MemberServiceRequest);
+
     public override async Task OnPageHandlerExecutionAsync(
         PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
     {
