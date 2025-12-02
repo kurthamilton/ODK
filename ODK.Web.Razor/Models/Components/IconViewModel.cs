eff896df492379f17f55bd1629f0856b0113181e
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2025-12-02 07:17:06 +0000

    WIP

--- ODK.Web.Razor/Models/Components/IconViewModel.cs
+++ ODK.Web.Razor/Models/Components/IconViewModel.cs
@@ -7,24 +7,7 @@ public class IconViewModel
         Type = type;
     }
 
-    public IDictionary<string, object>? Attributes { get; init; }
-
     public string? Class { get; init; }    
 
     public IconType Type { get; }
-
-    // Use static string to allow Icon components to use optional models.
-    // If a component uses a strongly-typed model, an instance of that type must be passed in,
-    // So leaving the model as dynamic allows for optional models.
-    public static string? GetClass(dynamic model)
-    {
-        var viewModel = model as IconViewModel;
-        return viewModel?.Class;
-    }
-
-    public static IDictionary<string, object>? GetAttributes(dynamic model)
-    {
-        var viewModel = model as IconViewModel;
-        return viewModel?.Attributes;
-    }
 }
