6e16df3f4725020cc157edf6443641e2d74fdf19
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2026-01-12 10:57:49 +0000

    Use login form styling for forgotten password

---
+++ ODK.Web.Razor/Models/Login/LoginBodyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+using ODK.Web.Common.Components;
+
+namespace ODK.Web.Razor.Models.Login;
+
+public class LoginBodyViewModel
+{
+    public IReadOnlyCollection<MenuItem>? Breadcrumbs { get; init; }
+
+    public IHtmlContent? Content { get; set; }
+
+    public Func<object?, IHtmlContent>? ContentFunc { get; set; }
+
+    public required string Title { get; init; }
+}
