250587f6304b8977dc109ff480927a0ff794fc41
Kurt Hamilton <kurt@littlewheel.com>
2026-07-11 11:29:56 +0100

    tinymce version bump; open batch file processes in tabs; patch third-party vulnerability; add readme

--- run-odk.bat
+++ run-odk.bat
@@ -1,4 +1,3 @@
-cd ODK.Web.Razor
-set DOTNET_WATCH_SUPPRESS_STATIC_FILE_HANDLING=true
-call npm run dev:odk
-pause
+@echo off
+rem Launch dotnet and sass as two tabs in a single Windows Terminal window.
+wt -d "%~dp0ODK.Web.Razor" --title "ODK dotnet (odk)" --suppressApplicationTitle cmd /k "set DOTNET_WATCH_SUPPRESS_STATIC_FILE_HANDLING=true && dotnet watch --urls=http://localhost:8124" ; new-tab -d "%~dp0ODK.Web.Razor" --title "ODK sass" --suppressApplicationTitle cmd /k "npm run watch:sass" ; focus-tab -t 0
