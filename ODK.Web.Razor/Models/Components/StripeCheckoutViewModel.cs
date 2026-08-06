637108d961da7335cf540ca48feb19027d9375ab
kurthamilton <kurt.j.hamilton@gmail.com>
2026-08-06 22:14:00 +0100

    Move checkout form into component

---
+++ ODK.Web.Razor/Models/Components/StripeCheckoutViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ODK.Web.Razor.Models.Components;
+
+public class StripeCheckoutViewModel
+{
+    public required string ApiPublicKey { get; init; }
+
+    public required string ClientSecret { get; init; }
+}
