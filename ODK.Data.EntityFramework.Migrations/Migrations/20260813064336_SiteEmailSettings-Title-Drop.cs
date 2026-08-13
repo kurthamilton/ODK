aff4349e8bbea77e675e6f73f33b6a62be652d3c
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2026-08-13 07:49:35 +0100

    (email admin): drop SiteEmailSettings.Title (#219)

---
+++ ODK.Data.EntityFramework.Migrations/Migrations/20260813064336_SiteEmailSettings-Title-Drop.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ODK.Data.EntityFramework.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class SiteEmailSettingsTitleDrop : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "SiteEmailSettings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "SiteEmailSettings",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
