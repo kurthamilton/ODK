b8e90b75c262d825144c276de5f9f84675e5b41b
kurthamilton <kurt.j.hamilton@gmail.com>
2026-08-23 22:41:07 +0100

    (groups): index layout + build-driven client-side compilation

--- ODK.Web.Razor/Models/Components/PageTitleViewModel.cs
+++ ODK.Web.Razor/Models/Components/PageTitleViewModel.cs
@@ -4,6 +4,8 @@ namespace ODK.Web.Razor.Models.Components;
 
 public class PageTitleViewModel
 {
+    public string? ContainerClass { get; init; }
+
     public IHtmlContent? Content { get; init; }
 
     public string? Title { get; init; }
