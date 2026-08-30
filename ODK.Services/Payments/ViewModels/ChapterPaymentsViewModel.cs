f23d01da98310329bf6375df44c98294935d2c8c
kurthamilton <kurt.j.hamilton@gmail.com>
2026-08-30 14:27:42 +0100

    (admin): more consistent use of breadcrumbs

--- ODK.Services/Payments/ViewModels/ChapterPaymentsViewModel.cs
+++ ODK.Services/Payments/ViewModels/ChapterPaymentsViewModel.cs
@@ -7,5 +7,11 @@ public class ChapterPaymentsViewModel
 {
     public required Chapter Chapter { get; init; }
 
+    /// <summary>
+    /// Whether the group has a payment account it can take money through. False while onboarding is
+    /// unfinished as well as before it starts, since neither state can be charged against.
+    /// </summary>
+    public required bool PaymentAccountEnabled { get; init; }
+
     public required IReadOnlyCollection<PaymentMemberDto> Payments { get; init; }
 }
